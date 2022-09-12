ccf63dec6f84c9d54b6382b368cba7c0950b8f55
Alfonso <vivancos@outlook.es>
2022-09-12 12:00:41 +0200

    Typo error
    'iptables' word appeared on two checks. The correcto is iptables and ipset

--- blocklist.sh
+++ blocklist.sh
@@ -14,7 +14,7 @@ abuse_key="INSERT_YOUR_API_KEY_HERE" #https://www.abuseipdb.com/account/api
 #if [ -f $IPTABLES_PATH ]; then echo "iptables OK"; else echo "Cannot find [ iptables ]. Is it installed? Exiting"; exit 1; fi;
 if ! command -v iptables >/dev/null; then  echo "I require iptables but it's not installed."; apt install -y iptables; else echo "iptables OK"; fi;
 #if [ -f $IPSET_PATH ]; then echo "ipset OK"; else echo "Cannot find [ ipset ]. Is it installed? Exiting"; exit 1; fi;
-if ! command -v ipset >/dev/null; then  echo "I require ipset but it's not installed."; apt install -y ipset; else echo "iptables OK"; fi;
+if ! command -v ipset >/dev/null; then  echo "I require ipset but it's not installed."; apt install -y ipset; else echo "ipset OK"; fi;
 #if [ -f $SORT_PATH ]; then echo "sort OK"; else echo "Cannot find [ sort ]. Is it installed? Exiting"; exit 1; fi;
 if ! command -v sort >/dev/null; then  echo "I require sort but it's not installed."; else echo "sort OK"; fi;
 #if [ -x $JQ_PATH ]; then echo "jq OK"; else echo "jq not installed, installing"; apt install -y jq; fi;
